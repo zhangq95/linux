d191db77c72e86f7f26ee3b5deafcd752932e2df
zhangq95 <qiangzh.hust@gmail.com>
2018-04-26 11:37:34 +0800

    update cgroup.c

--- include/linux/pid_namespace.h
+++ include/linux/pid_namespace.h
@@ -100,7 +100,7 @@ extern struct pid_namespace *task_active_pid_ns(struct task_struct *tsk);
 void pidhash_init(void);
 void pid_idr_init(void);
 
-/* Determine if task is in root_namespace or not */
+/* Determine if task is in root_namespace */
 static inline bool in_noninit_pid_ns(struct task_struct *tsk)
 {
 	return task_active_pid_ns(tsk) != &init_pid_ns;
